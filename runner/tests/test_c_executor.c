5aa89c9de668bc8bfd5f8254670641ba06b1024a
Joshua Weinstein <jweinstein@splunk.com>
2019-01-09 12:31:06 -0800

    executor unit tests

---
+++ runner/tests/test_c_executor.c
@@ -0,0 +1,14 @@
+#include "test_failures.h"
+#include "c_executor.h"
+
+void test_c_executor_compile_run(void)
+{
+
+}
+
+
+int main(int argc, char const *argv[])
+{
+	test_c_executor_compile_run();
+	TEST_FAIL_RETURN
+}
