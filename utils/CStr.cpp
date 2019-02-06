72f66f420fd9c2fd786602210d6222878495a18f
Joshua Weinstein <jweinstein@splunk.com>
2019-02-05 19:51:34 -0800

    cstr work

--- utils/CStr.cpp
+++ utils/CStr.cpp
@@ -1,3 +1,4 @@
+#include <cstring>
 #include "CStr.h"
 
 #ifndef MSTR_DEFAULT_CAP
@@ -6,7 +7,27 @@
 
 CStr::CStr(): _cap(MSTR_DEFAULT_CAP),
               _len(0),
-              _str(static_cast<char*>(std::malloc(_cap)))
+              _str(static_cast<char*>(std::malloc(_cap + 1))) /* + 1 for null char*/
 {
+	std::memset(_str, 0, _cap + 1);
+}
 
+CStr::CStr(const char* string)
+{
+	_len = std::strlen(string);
+	_cap = _len + MSTR_DEFAULT_CAP;
+	_str = static_cast<char*>(std::malloc(_cap + 1));
+	std::strncpy(_str, string, _cap + 1);
+}
+
+CStr::CStr(const CStr& other): _cap(other.getCap()),
+                               _len(other.getLen()),
+                               _str(static_cast<char*>(std::malloc(_cap + 1)))
+{
+	std::strncpy(_str, other.getStr(), _cap + 1);
+}
+
+CStr::~CStr()
+{
+		std::free(_str);
 }
--- utils/CStr.h
+++ utils/CStr.h
@@ -1,18 +1,25 @@
 #ifndef _UTIL_CSTR_H
 #define _UTIL_CSTR_H
 
-#include <cstring>
 #include <cstdlib>
 
 /*
  * String class that uses malloc/rellaoc/free for memory management.
- * Faster performance is primary purpose
+ * Additionally, this string class will attempt to reuse existing memory,
+ * such as if a new string is written into an old string.
  */
 
 class CStr 
 {
 public:
 	CStr();
+	explicit CStr(const char* string);
+	CStr(const CStr& other);
+	virtual ~CStr();
+
+	size_t getLen() const { return _len; }
+	size_t getCap() const { return _cap; }
+	const char* getStr() const { return _str; }
 private:
 	size_t _cap;
 	size_t _len;
